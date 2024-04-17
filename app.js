f70cb5b7c8d7adebcb2a56a1f0b121ed6bab9514
shreekanth7899 <sharmashreekanth@gmail.com>
2024-04-17 12:58:18 +0530

    Comments added for Reference

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Imports of Required Dependencies.
+
 const express = require('express')
 const path =  require('path');
 const { Socket } = require('socket.io');
@@ -8,8 +10,12 @@ const PORT = process.env.PORT || 4000 //const PORT = 4000
 const server = app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
 const io = require('socket.io')(server)
 
+
+// Path joining to Use the Public directory.
+
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Socket.io Connection details.
 io.on('connection', (Socket) => {
     console.log(Socket.id)
 })
@@ -21,11 +27,11 @@ function onConnected(socket) {
     console.log(socket.id)
     socketConnected.add(socket.id)
 
-    io.emit('Clients-total', socketConnected.size)
+    io.emit('clients-total', socketConnected.size)
 
     socket.on('disconnect', () => {
         console.log('socket disconnected', socket.id)
         socketConnected.delete(socket.id)
-        io.emit('Clients-total', socketConnected.size)
+        io.emit('clients-total', socketConnected.size)
     })
 }
