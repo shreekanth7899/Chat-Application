fadc7e2ab6f30311120e5130ba82c00af8d1b76a
shreekanth7899 <sharmashreekanth@gmail.com>
2024-04-17 12:58:41 +0530

    main.js update

--- public/main.js
+++ public/main.js
@@ -1,5 +1,20 @@
 const socket = io()
 
-socket.on('Clients-total', (data) => {
-    console.log(data)
+const clientsTotal = document.getElementById('client-total')
+const messageContainer = document.getElementById('message-container')
+const nameInput = document.getElementById('name-input')
+const messageForm = document.getElementById('message-form')
+const messageInput = document.getElementById('message-input')
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendMessage()
 })
+
+socket.on('clients-total', (data) => {
+    clientsTotal.innerText = 'Total clients : ' + data
+})
+
+function sendMessage(){
+    console.log(messageInput.value)
+}
